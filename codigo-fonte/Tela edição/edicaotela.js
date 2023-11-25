7375d60a9bcdc657969a4a376323a4452d1b64a1
Fernanda-Alice <famsoares@sga.pucminas.br>
2023-11-25 14:21:27 -0300

    update tela edição js
    update tela edição js

--- codigo-fonte/Tela edição/edicaotela.js
+++ codigo-fonte/Tela edição/edicaotela.js
@@ -1,34 +1,36 @@
-// Botão editar imagem
+
 function exibirImagem() {
-  var input = document.getElementById('arquivo'); 
-  var imagemContainer = document.getElementById('areaimagem'); 
- 
-  if (input.files && input.files[0]) { 
-      var leitor = new FileReader(); 
+  var input = document.getElementById('arquivo');
+  var imagemContainer = document.getElementById('areaimagem');
+
+  if (input.files && input.files[0]) {
+      var leitor = new FileReader();
+
       leitor.onload = function (e) {
-        var imagem = document.createElement('img');
-        imagem.src = e.target.result;
-    
-        imagemContainer.innerHTML = '';
-        imagemContainer.appendChild(imagem);
-    
-        var blob = dataURItoBlob(leitor.result);
-    
-        var url = URL.createObjectURL(blob);
-    
-        localStorage.setItem('imageURL', url);
-    };
-    
-    function dataURItoBlob(dataURI) {
-        var byteString = atob(dataURI.split(',')[1]);
-        var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
-        var ab = new ArrayBuffer(byteString.length);
-        var ia = new Uint8Array(ab);
-        for (var i = 0; i < byteString.length; i++) {
-            ia[i] = byteString.charCodeAt(i);
-        }
-        return new Blob([ab], { type: mimeString });
-    }
+          var imagem = document.createElement('img');
+          imagem.src = e.target.result;
+
+          imagemContainer.innerHTML = '';
+          imagemContainer.appendChild(imagem);
+
+          // Salvar a imagem no localStorage
+          var blob = dataURItoBlob(leitor.result);
+          var url = URL.createObjectURL(blob);
+          localStorage.setItem('imageURL', url);
+      };
+
+      function dataURItoBlob(dataURI) {
+          var byteString = atob(dataURI.split(',')[1]);
+          var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+          var ab = new ArrayBuffer(byteString.length);
+          var ia = new Uint8Array(ab);
+          for (var i = 0; i < byteString.length; i++) {
+              ia[i] = byteString.charCodeAt(i);
+          }
+          return new Blob([ab], { type: mimeString });
+      }
+
+      leitor.readAsDataURL(input.files[0]);
   }
 }
 
@@ -38,28 +40,34 @@ function clicou() {
   var descricao = document.querySelector('#descricao').value;
   var preco = document.getElementById('preco').value;
 
-
-  var imageData = localStorage.getItem('imageData');
-
- 
-
+  // Salvar os dados no localStorage
   localStorage.setItem('preco', preco);
   localStorage.setItem('nomedoproduto', nomedoproduto);
   localStorage.setItem('descricao', descricao);
+
+  // Recuperar a imagem do localStorage e exibir novamente
+  var imageURL = localStorage.getItem('imageURL');
+  if (imageURL) {
+      var imagemContainer = document.getElementById('areaimagem');
+      var imagem = document.createElement('img');
+      imagem.src = imageURL;
+      imagemContainer.innerHTML = '';
+      imagemContainer.appendChild(imagem);
+  }
 }
 
-
 window.onload = function () {
+  // Preencher os campos com os dados do localStorage ao carregar a página
   document.getElementById('preco').value = localStorage.getItem('preco');
   document.getElementById('nomedoproduto').value = localStorage.getItem('nomedoproduto');
   document.getElementById('descricao').value = localStorage.getItem('descricao');
 
-//mostra image no local storage
-  var imageData = localStorage.getItem('imageData');
-  if (imageData) {
+  // Exibir a imagem do localStorage
+  var imageURL = localStorage.getItem('imageURL');
+  if (imageURL) {
       var imagemContainer = document.getElementById('areaimagem');
       var imagem = document.createElement('img');
-      imagem.src = 'data:image/png;base64,' + imageData; // Assuming it's a PNG image
+      imagem.src = imageURL;
       imagemContainer.innerHTML = '';
       imagemContainer.appendChild(imagem);
   }
@@ -67,18 +75,14 @@ window.onload = function () {
 
 // Botão excluir item
 function deleteItem() {
-  // Clear local storage LIMPA
+  // Limpar o localStorage
   localStorage.clear();
 
-  // Reset os campos
+  // Resetar os campos e a imagem
   document.getElementById('preco').value = '';
   document.getElementById('nomedoproduto').value = '';
   document.getElementById('descricao').value = '';
-  document.getElementById('areaimagem').innerHTML = ''; // Climpa o container
-
-  location.reload
-  location.href = '/codigo-fonte/menu/menu.html'
-
+  document.getElementById('areaimagem').innerHTML = ''; // Limpar o container
 }
 
  
