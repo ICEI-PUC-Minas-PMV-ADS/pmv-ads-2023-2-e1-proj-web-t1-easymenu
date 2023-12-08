3572d313370c554ebb1869f6d56d9b52745bb0f8
Lourencodevs <talysson.lourenco@sga.pucminas.br>
2023-12-07 21:52:32 -0300

    Alterações do Projeto Etapa 5
    Alterações feitas conforme solicitação Joyce

--- codigo-fonte/Development EasyMenu/js/menu.js
+++ codigo-fonte/Development EasyMenu/js/menu.js
@@ -21,46 +21,10 @@ var MENU = {
         "id": "3",
         "name": "CheeseBurgerBacon",
         "img": "../imagens/hamb3.jpg",
-        "name": "Cheesebacon",
+        "name": "CheeseBurgerBacon",
         "dsc": "Pão da sua escolha, 2 hamburguers de 180g de carne bovina e duas fatias de cheddar.",
         "price": 35
       },
-      
-      {
-        "id": "4",
-         "name": "Gramercy Tavern",
-         "img": "./img/cardapio/burguers/Gramercy-Tavern-Burger-and-Kielbasa-Kit-6.4.21-72ppi-1x1-15.jpg",
-         "dsc": "The Gramercy Tavern Burger - 4 Pack",
-         "price": 49
-       },
-       {
-        "id": "shake-shack-shackburger-8-pack",
-        "img": "./img/cardapio/burguers/shake-shack-shackburger-8-pack.973a5e26836ea86d7e86a327becea2b0.jpg",
-        "name": "Shake Shack",
-        "dsc": "Shake Shack ShackBurger® - 8 Pack",
-        "price": 49
-    },
-    {
-        "id": "gotts-cheeseburger-kit-for-4",
-        "img": "./img/cardapio/burguers/gotts-complete-cheeseburger-kit-for-4.7bdc74104b193427b3fe6eae39e05b5e.jpg",
-        "name": "Gott's Roadside",
-        "dsc": "Gott's Complete Cheeseburger Kit for 4",
-        "price": 39
-    },
-    {
-        "id": "le-big-matt-kit-for-6",
-        "img": "./img/cardapio/burguers/le-big-matt-kit-for-6.1ddae6e382bb3218eeb0fd5247de115a.jpg",
-        "name": "Emmy Squared",
-        "dsc": "Le Big Matt Burger Kit for 6",
-        "price": 49
-    },
-    {
-        "id": "shake-shack-shackburger-16-pack",
-        "img": "./img/cardapio/burguers/shake-shack-shackburger-16-pack.316f8b09144db65931ea29e34869287a.jpg",
-        "name": "Shake Shack",
-        "dsc": "Shake Shack Shackburger® - 16 Pack",
-        "price": 39
-    },
   
     ],
   
@@ -73,118 +37,10 @@ var MENU = {
         "dsc": "330g de batata frita.",
         "price": 20
       },
-  
-      {
-        "id": "dry-aged-boneless-ribeye-steak-dinner-kit-for-4",
-        "name": "Chef Francis Mallmann",
-        "img": "./img/cardapio/steaks/dry-aged-boneless-ribeye-steak-dinner-for-4.81c3bdc05fe6bdb2c2214709863120e0.jpg",
-        "dsc": "Dry-Aged Boneless Ribeye Steak Dinner Kit for 4",
-        "price": 75
-    },
-    {
-        "id": "mesquite-smoked-peppered-beef-tenderloin",
-        "img": "./img/cardapio/steaks/mesquite-smoked-peppered-beef-tenderloin.5c314418a1f75c7057eed686e2fad46f.jpg",
-        "name": "Perini Ranch Steakhouse",
-        "dsc": "Mesquite Smoked Peppered Beef Tenderloin",
-        "price": 65
-    },
-    {
-        "id": "Prime-holiday-steak-sampler-for-10-12",
-        "img": "./img/cardapio/steaks/holiday-sampler-9-pack.2de75ca80282ffe9d064eb757ff7a5a1.jpg",
-        "name": "Saltbrick Prime",
-        "dsc": "Chef Matt's Steak Sampler for 10-12",
-        "price": 79
-    },
 
     
 ],
 
-"pizzas": [
-    {
-        "id": "2-lou-malnatis-deep-dish-pizzas",
-        "img": "./img/cardapio/pizzas/2-lou-malnatis-deep-dish-pizzas.bf0fe065d251a9cca3925b269d443a27.jpg",
-        "name": "Lou Malnati's Pizza",
-        "dsc": "2 Lou Malnati's Deep Dish Pizzas",
-        "price": 67.99
-    },
-    {
-        "id": "23699-choose-your-own-thin-crust-pizza-4-pack",
-        "img": "./img/cardapio/pizzas/choose-your-own-thin-crust-pizza-4-pack.b928a2008eab50c65dc87e59b5952190.jpg",
-        "name": "Bartolini's",
-        "dsc": "Choose Your Own Thin Crust Pizza - 4 Pack",
-        "price": 139
-    },
-    {
-        "id": "choose-your-own-new-haven-style-pizza-6-pack",
-        "img": "./img/cardapio/pizzas/choose-your-own-new-haven-style-pizza-6-pack.ab82828afc6172cdd4017556c15e36dd.jpg",
-        "name": "Zuppardi's Apizza",
-        "dsc": "New Haven-Style Pizza - 6 Pack (Choose Your Own)",
-        "price": 79
-    },
-    {
-        "id": "6-lou-malnatis-deep-dish-pizzas",
-        "img": "./img/cardapio/pizzas/6-lou-malnatis-deep-dish-pizzas.f59993181da5d295668c8a6fb856055e.jpg",
-        "name": "Lou Malnati's Pizza",
-        "dsc": "6 Lou Malnati's Deep Dish Pizzas",
-        "price": 116.99
-    },
-    {
-        "id": "wood-fired-pizzas-best-seller-4-pack",
-        "img": "./img/cardapio/pizzas/wood-fired-pizzas-best-seller-4-pack.1653bb05922ba153ac178f8365d27f6d.jpg",
-        "name": "Pizzeria Bianco",
-        "dsc": "Wood Fired Pizzas Best Seller - 4 Pack",
-        "price": 129
-    },
-    {
-        "id": "236991-choose-your-own-deep-dish-pizza-3-pack",
-        "img": "./img/cardapio/pizzas/choose-your-own-deep-dish-pizza-3-pack.4111791511244a4946bb5c9ad2c17da9.jpg",
-        "name": "Bartolini's",
-        "dsc": "Choose Your Own Deep Dish Pizza - 3 Pack",
-        "price": 139
-    },
-    {
-        "id": "choose-your-own-detroit-style-pizza-3-pack",
-        "img": "./img/cardapio/pizzas/detroit-style-pizza-choose-your-own-3-pack.6b6f4909ffd4066d5471e70eac5c3d89.jpg",
-        "name": "Emmy Squared",
-        "dsc": "Detroit-Style Pizza - Choose Your Own 3 Pack",
-        "price": 89
-    },
-    {
-        "id": "brooklyn-pizza-choose-your-own-5-pack",
-        "img": "./img/cardapio/pizzas/brooklyn-pizza-choose-your-own-5-pack.edc4f476a75207d0af840ce6f225f2b3.jpg",
-        "name": "Paesan's Pizza",
-        "dsc": "Brooklyn Pizza - Choose Your Own 5 Pack",
-        "price": 69
-    },
-    {
-        "id": "choose-your-own-chicago-deep-dish-pizza-4-pack",
-        "img": "./img/cardapio/pizzas/chicago-deep-dish-pizza-4-pack.49927daafa8c147fe9bb2a113e56668e.jpg",
-        "name": "My Pi Pizza",
-        "dsc": "Chicago Deep Dish Pizza - 4 Pack",
-        "price": 129
-    },
-    {
-        "id": "4-lou-malnatis-deep-dish-pizzas",
-        "img": "./img/cardapio/pizzas/4-lou-malnatis-deep-dish-pizzas.8c79eb7506b5752ab3387d8174246b17.jpg",
-        "name": "Lou Malnati's Pizza",
-        "dsc": "4 Lou Malnati's Deep Dish Pizzas",
-        "price": 96.99
-    },
-    {
-        "id": "tonys-custom-pizza-3-pack",
-        "img": "./img/cardapio/pizzas/choose-your-own-pizza-3-pack.fcf7a43e38593007ef2857fe16d6dd26.jpg",
-        "name": "Tony's Pizza Napoletana",
-        "dsc": "Choose Your Own Pizza - 3 Pack",
-        "price": 99
-    },
-    {
-        "id": "plain-thin-crust-pizza-4-pack",
-        "img": "./img/cardapio/pizzas/plain-thin-crust-pizza-4-pack.5540e9d166db2f0853643c6517e4a225.jpg",
-        "name": "The Columbia Inn",
-        "dsc": "Plain Thin Crust Pizza - 4 Pack",
-        "price": 79
-    }
-],
 
 "sobremesas": [
     {
@@ -194,90 +50,6 @@ var MENU = {
       "dsc": "Cheesecake com calda de sua preferência.",
       "price": 15
     },
-    {
-        "id": "15259-german-chocolate-killer-brownie-tin-pack",
-        "img": "./img/cardapio/sobremesas/german-chocolate-killer-brownie-tin-pack.5ebc34160f28767a9d94c4da2e04c4b9.jpg",
-        "name": "Killer Brownie®",
-        "dsc": "German Chocolate Killer Brownie®",
-        "price": 39.99
-    },
-    {
-        "id": "jacques-world-famous-chocolate-chip-cookies",
-        "img": "./img/cardapio/sobremesas/jacques-world-famous-chocolate-chip-cookies-6-pack.2217a14c443602493bba88aa9335319a.jpg",
-        "name": "Jacques Torres Chocolate",
-        "dsc": "Jacques' World Famous Chocolate Chip Cookies - 6 Pack",
-        "price": 39.95
-    },
-    {
-        "id": "luigis-original-cannoli-pie",
-        "img": "./img/cardapio/sobremesas/original-cannoli-pie.4cb5b9ba82f57b69b90765fd9f07aa1a.jpg",
-        "name": "The Cannoli Pie Company",
-        "dsc": "Original Cannoli Pie",
-        "price": 69
-    },
-    {
-        "id": "sea-salted-caramel-swirl-cheesecake",
-        "img": "./img/cardapio/sobremesas/sea-salted-caramel-swirl-cheesecake.e2825335433fb7a272a5d77649a6849e.jpg",
-        "name": "Cotton Blues Cheesecake Company",
-        "dsc": "Sea-Salted Caramel Swirl Cheesecake",
-        "price": 65
-    },
-    {
-        "id": "brooklyn-blackout-cookie-brownie-combo-pack-2-tins",
-        "img": "./img/cardapio/sobremesas/brooklyn-blackout-cookie-brownie-combo-pack-2-tins.d8805325baf6b23b4f01d119dc4531a7.jpg",
-        "name": "Brooklyn Blackout Company",
-        "dsc": "Brooklyn Blackout Cookie + Brownie Combo Pack - 2 Tins",
-        "price": 89
-    },
-    {
-        "id": "best-seller-cupcake-dozen",
-        "img": "./img/cardapio/sobremesas/best-seller-cupcake-dozen.f93b21993f6a2da11c975d45b9b0d08f.jpg",
-        "name": "Crave Cupcakes",
-        "dsc": "Best Seller Cupcake Dozen",
-        "price": 89
-    },
-    {
-        "id": "choose-your-own-ice-cream-donuts-6-pack",
-        "img": "./img/cardapio/sobremesas/choose-your-own-ice-cream-donuts-6-pack.24d0b44765a7c54237fcd7ea9d9d8093.jpg",
-        "name": "Elegant Desserts",
-        "dsc": "Choose Your Own Ice Cream Donuts - 6 Pack",
-        "price": 69
-    },
-    {
-        "id": "17481-jewish-dessert-3-pack",
-        "img": "./img/cardapio/sobremesas/jewish-classics-dessert-pack.7d4b76630f2fe75dcb7bbcf2501b7390.jpg",
-        "name": "Ess-a-Bagel",
-        "dsc": "Jewish Classics Dessert Pack",
-        "price": 89.95
-    },
-    {
-        "id": "dessert-bar-care-package",
-        "img": "./img/cardapio/sobremesas/dessert-bar-care-package.324aa28fe98c9dc67b75aac43376902e.jpg",
-        "name": "Bread and Roses Bakery",
-        "dsc": "Dessert Bar Care Package",
-        "price": 65
-    },
-    {
-        "id": "donut-cookies-12-pack",
-        "img": "./img/cardapio/sobremesas/donut-cookies-12-pack.46f42c61c4a79fd2051a99b2f23e890e.jpg",
-        "name": "Stan's Donuts",
-        "dsc": "Donut Cookies - 12 Pack",
-        "price": 49
-    },
-    {
-        "id": "gulab-jamun-ice-cream-cakes-2-pack",
-        "img": "./img/cardapio/sobremesas/gulab-jamun-ice-cream-cakes-2-pack.c45b4d0750ad22d741f84dc1f26d20e7.jpg",
-        "name": "Malai Ice Cream",
-        "dsc": "Gulab Jamun Ice Cream Cakes - 2 Pack",
-        "price": 79
-    },
-    {
-        "id": "jacques-world-famous-chocolate-chip-cookies-12-pack",
-        "img": "./img/cardapio/sobremesas/jacques-world-famous-chocolate-chip-cookies-12-pack.3b373bdd67cd25084182c21499f675d1.jpg",
-        "name": "Jacques Torres Chocolate",
-        "dsc": "Jacques' World Famous Chocolate Chip Cookies - 12 Pack",
-        "price": 69.95
-    }
   ],
 
   "bebidas": [
@@ -288,90 +60,6 @@ var MENU = {
       "dsc": "Refrigerante em lata",
       "price": 5
     },
-    {
-        "id": "hong-kong-boba-tea-kit-for-6",
-        "img": "./img/cardapio/bebidas/hong-kong-boba-tea-kit-for-6.63841de36d8e5edfafa13023fc303285.jpg",
-        "name": "New Territories",
-        "dsc": "Hong Kong Boba Tea Kit for 6",
-        "price": 59
-    },
-    {
-        "id": "guys-caliente-margaritas-for-12",
-        "img": "./img/cardapio/bebidas/guys-caliente-margaritas-for-12.ca8c6bc06b8f1039549385ffcebc749d.jpg",
-        "name": "Guy Fieri",
-        "dsc": "Guy's Caliente Margaritas for 12",
-        "price": 69
-    },
-    {
-        "id": "woodford-reserve-mint-julep-syrup",
-        "img": "./img/cardapio/bebidas/woodford-reserve-mint-julep-syrup.ef523ac7cbae5f4aba6b058207f490d2.jpg",
-        "name": "Woodford Reserve",
-        "dsc": "Woodford Reserve Mint Julep Syrup",
-        "price": 39
-    },
-    {
-        "id": "new-orleans-hurricane-mix",
-        "img": "./img/cardapio/bebidas/new-orleans-hurricane-mix.4613584fc65cb0787024dd24d2a8f4b3.jpg",
-        "name": "Franco's Hurricane Mix",
-        "dsc": "New Orleans Hurricane Mix",
-        "price": 39
-    },
-    {
-        "id": "margarita-mix",
-        "img": "./img/cardapio/bebidas/margarita-mix.bd48a000d589d3147b14790af3c33fcd.jpg",
-        "name": "Johnny Sanchez",
-        "dsc": "Margarita Mix",
-        "price": 59
-    },
-    {
-        "id": "woodford-reserve-mint-julep-syrup-2-pack",
-        "img": "./img/cardapio/bebidas/woodford-reserve-mint-julep-syrup-2-pack.0ac76063f151988113cbaabd0eaa829f.jpg",
-        "name": "Woodford Reserve",
-        "dsc": "Woodford Reserve Mint Julep Syrup - 2 Pack",
-        "price": 59
-    },
-    {
-        "id": "unicorn-parade-milkshake-kit-for-8",
-        "img": "./img/cardapio/bebidas/unicorn-parade-milkshake-kit-for-2.9052d04c1cf25b29442048bd3e535f21.jpg",
-        "name": "New Territories",
-        "dsc": "Unicorn Parade Milkshake Kit for 8",
-        "price": 109
-    },
-    {
-        "id": "chickpea-chiller-kit-for-6",
-        "img": "./img/cardapio/bebidas/chickpea-chiller-kit-for-6.4310765c71ba524b5462ea9330d32446.jpg",
-        "name": "The Hummus & Pita Co.",
-        "dsc": "Chickpea Chiller Kit for 6",
-        "price": 89
-    },
-    {
-        "id": "15194-old-honey-barn-mint-julep-mixer-200ml",
-        "img": "./img/cardapio/bebidas/old-honey-barn-mint-julep-mixer-200ml.e0b131d6d9b69963706b43fd4334ab74.jpg",
-        "name": "Old Honey Barn Mint Julep",
-        "dsc": "Old Honey Barn Mint Julep Mixer - 200ml Flask",
-        "price": 25
-    },
-    {
-        "id": "kentucky-derby-mint-julep-gift-set",
-        "img": "./img/cardapio/bebidas/kentucky-derby-mint-julep-gift-set.79720eda4e9c8e3fcf9ecb5c79827f2c.jpg",
-        "name": "Woodford Reserve",
-        "dsc": "Kentucky Derby Mint Julep Gift Set",
-        "price": 59.95
-    },
-    {
-        "id": "002-charleston-bloody-mary-mix-weekender-bold-and-spicy",
-        "img": "./img/cardapio/bebidas/weekender-charleston-bloody-mary-mix-bold-and-spicy.c372868c9937e407a299a22001e210e2.jpg",
-        "name": "Charleston Beverage Company",
-        "dsc": "Weekender | Charleston Bloody Mary Mix Bold & Spicy",
-        "price": 39.95
-    },
-    {
-        "id": "nola-cold-brew-concentrate-bag-in-box",
-        "img": "./img/cardapio/bebidas/nola-cold-brew-concentrate-bag-in-box.3df6fde8bd83f29235565984ae8ed22b.jpg",
-        "name": "Grady's Cold Brew",
-        "dsc": "NOLA Cold Brew Concentrate Bag-in-Box",
-        "price": 40
-    }
   ],
 }
   
